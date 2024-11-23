df8eca7dd891711d66e055e276793d67f303bbc6
Ondra <access2tools@gmail.com>
2024-11-23 03:36:44 -0800

    MIME

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
-import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   base: '/cart-dept/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    },
+    modulePreload: {
+      polyfill: true
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+    extensions: ['.js', '.jsx', '.json']
+  }
 })
